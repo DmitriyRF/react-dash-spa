9a4f753e606fe36d2b07c4828b9177a49c7947bf
Sergey Popkov <4m9fka@gmail.com>
2018-08-13 12:44:36 +0300

    [C-6] added sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Sidebar from './components/Sidebar/Sidebar'
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,9 +11,16 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          <div className="container">
+              <Sidebar/>
+              <div className="content">
+                  <p className="App-intro">
+                      To get started, edit <code>src/App.js</code> and save to reload.
+                  </p>
+
+              </div>
+          </div>
+
       </div>
     );
   }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './Sidebar.css'
+ const Sidebar = () => {
+    return (
+        <div className="sidebar">
+                <div className="menu">
+                    <a href="#">Link 1</a>
+                    <a href="#">Link 2</a>
+                    <a href="#" className="active"> Link 23</a>
+                </div>
+        </div>
+    )
+};
+export default Sidebar
