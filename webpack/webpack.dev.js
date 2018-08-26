5bb2690e79e6578cdc11ae95c19bfb3f1ad103e8
Dima Romanovskiy <Dmitriy_r_f@mail.ru>
2018-08-27 02:31:25 +0300

    Add starting development setting, build not working yet

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,28 +1,79 @@
 const commonPaths = require('./paths');
 
 module.exports = {
+  entry: {
+    index: ['./src/index.js']
+  },
   mode: 'development',
   output: {
     filename: '[name].js',
     path: commonPaths.outputPath,
-    chunkFilename: '[name].js',
+    chunkFilename: '[name].js'
+  },
+  serve: {
+    port: 3333,
+    open: true
   },
   module: {
     rules: [
       {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
         test: /\.(css|scss)$/,
         use: [
-          'style-loader',
+          {
+            loader: 'style-loader'
+          },
           {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              localIdentName: '[local]___[hash:base64:5]',
-            },
+              localIdentName: '[local]___[hash:base64:5]'
+            }
           },
-          'sass-loader',
-        ],
+          {
+            loader: 'sass-loader'
+          }
+        ]
       },
-    ],
-  },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
 };
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,13 +1,16 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const commonPaths = require('./paths');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const commonPaths = require('./paths');
 
 module.exports = {
+  entry: {
+    index: ['./src/index.js']
+  },
   mode: 'production',
   output: {
-    filename: `${commonPaths.jsFolder}/[name].[hash].js`,
+    filename: `[name].js`,
     path: commonPaths.outputPath,
-    chunkFilename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js'
   },
   module: {
     rules: [
@@ -19,22 +22,64 @@ module.exports = {
             loader: 'css-loader',
             options: {
               sourceMap: true,
-              localIdentName: '[local]___[hash:base64:5]',
-            },
+              localIdentName: '[local]___[hash:base64:5]'
+            }
           },
-          'sass-loader',
-        ],
+          {
+            loader: 'sass-loader'
+          }
+        ]
       },
-    ],
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].html'
+            }
+          },
+          {
+            loader: 'extract-loader'
+          },
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src']
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(jpg|jpeg|gif|png)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: 'images/[name]-[hash:8].[ext]'
+            }
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new CleanWebpackPlugin([commonPaths.outputPath.split('/').pop()], {
-      root: commonPaths.root,
+      root: commonPaths.root
     }),
     new MiniCssExtractPlugin({
       filename: `${commonPaths.cssFolder}/[name].css`,
-      chunkFilename: '[id].css',
-    }),
+      chunkFilename: '[id].css'
+    })
   ],
-  devtool: 'source-map',
+  devtool: 'source-map'
 };
