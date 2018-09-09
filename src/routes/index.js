3ece6fa1dd2c86df893bce43a50eea9ffdb29d66
4m9fka <4m9fka@gmail.com>
2018-09-09 20:01:17 +0300

    Auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 import Employees from '../pages/Employees';
 import FAQ from '../pages/FAQ';
 import Help from '../pages/Help';
 import Home from '../pages/Home';
 import Projects from '../pages/Projects';
+import Login from '../pages/Login';
 
 const Routes = () => (
   <Switch>
-    <Route path="/" exact component={Home} />
-    <Route path="/employees" component={Employees} />
-    <Route path="/projects" component={Projects} />
-    <Route path="/faq" component={FAQ} />
-    <Route path="/help" component={Help} />
+    <Route path="/login" component={Login} />
+
+      <PrivateRoute path="/" exact component={Home} />
+      <PrivateRoute path="/employees" component={Employees} />
+      <PrivateRoute path="/projects" component={Projects} />
+      <PrivateRoute path="/faq" component={FAQ} />
+      <PrivateRoute path="/help" component={Help} />
+
   </Switch>
 );
 export default Routes;
--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,4 +1,5 @@
 import company from 'Components/Sidebar/CompanySwitcher/reducers';
 import employees from '../pages/Employees/reducers';
+import auth from './auth/reducer';
 
-export { company, employees };
+export { company, employees,auth };
